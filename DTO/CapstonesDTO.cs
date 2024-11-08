bdd7b32872aa9076621e0c3acbf8571ecad8c944
Guiller Santos <155286241+guillersantos111@users.noreply.github.com>
2024-11-08 20:05:44 +0800

    Add files via upload

---
+++ DTO/CapstonesDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace CapstoneGenerator.Shared.Models
+{
+    public class CapstonesDTO
+    {
+        public int CapstoneId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Categories { get; set; }
+        public string CreatedBy { get; set; }
+        public float Rating { get; set; }
+    }
+}
